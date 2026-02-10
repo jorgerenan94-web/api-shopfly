d1533eea809a5f4cf5adbe6d7f5d8f8f05c8d999
JorgeRenan <jorgerenan94@gmail.com>
2026-02-10 12:36:44 -0300

    fix: corrigir formatação e parâmetros na função de criação de produtos

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,10 +1,10 @@
 const productsModel = require("../models/products");
 
 async function createProduct(req, res) {
-    const {name, price, category_id } = req.body
+    const { name, price, category_id } = req.body
     
     try {
-        const result = await productsModel.query(`INSERT INTO products (name, price, category_id) VALUES ($1, $2, $3) RETURNING *`);
+        const result = await productsModel.query(`INSERT INTO products (name, price, category_id) VALUES ($1, $2, $3) RETURNING *;`, [name, price, category_id]);
         res.status(201).send(result.rows[0])
     } catch (error) {
         console.error(`Erro ao criar produto:`, error)
