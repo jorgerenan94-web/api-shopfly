d7d3cb590ca0c1ef184c0e31c6c8ffccaed137c5
JorgeRenan <jorgerenan94@gmail.com>
2026-02-11 12:06:57 -0300

    Correção na rota de buscar categorias de produto

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const productsController = require("../controllers/products");
+const categoriesController = require("../controllers/categories");
 //const productsMiddleware = require("../middlewares/products");
 
-router.get("/products", productsController.getAllProducts)
+router.get("/categories", categoriesController.getAllCategories)
 
 module.exports = router;
