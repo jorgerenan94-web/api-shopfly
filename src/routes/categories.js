ff933fc3af30674ef603a679f3cdd16f3b34aed2
JorgeRenan <jorgerenan94@gmail.com>
2026-02-11 12:03:39 -0300

    Implementação da rota de buscar categorias de produto

---
+++ src/routes/categories.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const productsController = require("../controllers/products");
+//const productsMiddleware = require("../middlewares/products");
+
+router.get("/products", productsController.getAllProducts)
+
+module.exports = router;
