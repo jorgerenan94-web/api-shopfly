9b48211d4bb6a08ed12b6fda5b5be26fff936ab5
JorgeRenan <jorgerenan94@gmail.com>
2026-02-11 12:28:50 -0300

    Implementação do controlador de criação de categorias

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -2,7 +2,7 @@ const { categoriesModel } = require("../models/categories");
 
 async function getAllCategories(req, res){
     try {
-        const result = await categoriesModel.query(`SELECT * FROM categories`)
+        const result = await categoriesModel.query(`SELECT * FROM categories ORDER BY id DESC`)
 
         res.status(200).send(result.rows)
     } catch (error) {
@@ -11,6 +11,18 @@ async function getAllCategories(req, res){
     }
 }
 
+async function createCategory(req, res) {
+    const { name } = req.body
+    
+    try {
+        const result = await categoriesModel.query(`INSERT INTO categories name VALUES $1 RETURNING *;`, [name]);
+        res.status(201).send(result.rows[0])
+    } catch (error) {
+        console.error(`Erro ao criar categoria:`, error)
+        res.status(500).send({error: `Erro ao criar categoria`})
+    }
+}
 module.exports = {
-    getAllCategories
+    getAllCategories,
+    createCategory
 }
