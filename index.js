ef8a2a74ae5f1aaba55c9fc54e6861a7dea24005
JorgeRenan <jorgerenan94@gmail.com>
2026-02-11 12:09:11 -0300

    Inicialização da rota de buscar categorias do produto

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const database = require("./src/config/database");
 const routesProducts = require("./src/routes/products");
-const categoriesModel = require("./src/models/categories");
+const routesCategories = require("./src/routes/categories");
 
 const app = express();
 const port = 4606;
@@ -10,6 +10,7 @@ const port = 4606;
 app.use(express.json())
 
 app.use("/", routesProducts)
+app.use("/", routesCategories)
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`)
