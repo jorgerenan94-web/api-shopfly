fa8b9834e94383ec9c356812aa75a506dea73aa4
JorgeRenan <jorgerenan94@gmail.com>
2026-02-11 11:39:07 -0300

    Implementação da rota de atualização do produto

--- src/routes/products.js
+++ src/routes/products.js
@@ -7,5 +7,6 @@ const productsMiddleware = require("../middlewares/products");
 router.post("/products", productsMiddleware.validadeCreateProduct, productsController.createProduct)
 router.get("/products", productsController.getAllProducts)
 router.delete("/products/:id", productsMiddleware.validadeDeleteProduct, productsController.deleteProduct)
+router.put("/products/:id", productsMiddleware.validadeUpdateProduct, productsController.updateProduct)
 
 module.exports = router;
